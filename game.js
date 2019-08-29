04b246d4157bb69c413f9e19b8d92dca4b47679f
Hampus <hampus.gardstrom@live.dk>
2019-08-29 09:28:38 +0200

    Multiplayer revamped

--- game.js
+++ game.js
@@ -91,4 +91,33 @@ onkeyup = function (e) {
 //#endregion EVENTS
 
 
+
+function networkConnect() {
+    confirm("Please type the pin code")
+        var pin = promt("Pin: ")
+        socket = ElineSocket.connect(pin);
+    }
+    
+    function networkCreate(){
+        socket = ElineSocket.create();
+    }
+    
+    function networkRead(){
+        socket.onMessage(handleMessage);
+    }
+    
+    
+    function handleMessage(msg) {
+        switch (msg.type) {
+            case 'shootNew':
+                SceneCollection.findScene(Game).oScene.balls.push(gameNamespace.Objects.Presets.NewBall());
+                break;
+            default:
+                console.log('Unknown message', msg);
+        }
+    }
+    
+    
+
+
 }
